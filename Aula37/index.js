664342885d17916b927d3e9b9249dd5788ef3e06
Wendreo Oliveira <wendreo23@gmail.com>
2023-08-11 10:58:58 -0300

    feat: upload until class 39

--- Aula37/index.js
+++ Aula37/index.js
@@ -1,5 +1,5 @@
 //            0123...
-const nomes = ['Wendreo', 'Oliveira'];
+//const nomes = ['Wendreo', 'Oliveira'];
 
 // for (let i = 0; i < nome.length; i++) {
 //     console.log(nome[i]);
@@ -8,7 +8,17 @@ const nomes = ['Wendreo', 'Oliveira'];
 // for (let i in nome) {
 //     console.log(nome[i]);  // retornar o indice
 // }
+const pessoa = {
+    nome: 'Wendreo',
+    sobrenome: 'Oliveira'
+};
+// For clássico - Geralmente com iteráveis(array ou strings)
+// For in - retorna o índice ou chave(string, array ou objetos)
+// for of - retorna o valor em si (iteráveis, arrays ou strings)
 
-for (let valor of nomes) {
-    console.log(valor);
-}
+// for (let chave in pessoa) {
+//     console.log(chave, pessoa[chave]);
+// }
+// for (let valor of nomes) {
+//     console.log(valor);
+// }
