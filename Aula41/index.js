5a5aed4197cb955225ed2e8f6c0702e98acd1d95
Wendreo Oliveira <wendreo23@gmail.com>
2023-08-17 23:54:42 -0300

    feat: upload until class 41

---
+++ Aula41/index.js
@@ -0,0 +1,12 @@
+// Escreva uma função que recebe 2 números e retorne o maior deles
+
+// function max(x, y) {
+//     if (x > y) {
+//         return x;
+//     } else {
+//         return y;
+//     }
+// }
+
+const max2 = (x, y) => x > y ? x : y;
+console.log(max2(10, 2));
