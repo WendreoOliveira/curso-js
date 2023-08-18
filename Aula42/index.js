b886a06bbcbd805a99b74f584e212320056545f3
Wendreo Oliveira <wendreo23@gmail.com>
2023-08-18 02:43:25 -0300

    feat: upload until class 42

---
+++ Aula42/index.js
@@ -0,0 +1,15 @@
+/* Escreva uma função chamada ePaisagem
+que recebe dois argumentos, largura e altura
+de uma imagem (number).
+retorne true se a imagem estiver no modo
+paisagem. */
+
+// function ePaisagem(largura, altura) {
+//     return largura > altura; //? true : false;
+// }
+
+// console.log(ePaisagem(1080, 1920));
+
+const ePaisagem = (largura, altura) => largura > altura;
+
+console.log(ePaisagem(1080, 1920));
